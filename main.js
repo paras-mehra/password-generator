935c4a29328b1f47b39ec78f894ca832c7edb468
Paras Mehra <116555118+paras-mehra@users.noreply.github.com>
2024-04-03 19:11:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const upperSet = "ABCDEFGHIJKLMOPQRSTUVWYXZ";
+const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "~!@#$%^&*()_+/";
+
+// Selector
+const passBox = document.getElementById("pass-box");
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
+
+const getRandomData = (dataSet) => {
+  return dataSet[Math.floor(Math.random() * dataSet.length)];
+};
+
+const generatePassword = (password = "") => {
+  if (upperInput.checked) {
+    password += getRandomData(upperSet);
+  }
+  if (lowerInput.checked) {
+    password += getRandomData(lowerSet);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(numberSet);
+  }
+  if (symbolInput.checked) {
+    password += getRandomData(symbolSet);
+  }
+  if (password.length < totalChar.value) {
+    return generatePassword(password);
+  }
+  passBox.innerText = turncateString(password, totalChar.value);
+};
+
+generatePassword();
+
+document.getElementById("btn").addEventListener("click", () => {
+  generatePassword();
+});
+
+function turncateString(str, num) {
+  if (str.length > num) {
+    let subStr = str.substring(0, num);
+    return subStr;
+  } else {
+    return str;
+  }
+}
